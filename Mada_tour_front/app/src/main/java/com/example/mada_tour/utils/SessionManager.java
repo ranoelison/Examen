2b5f41a0c14392e39de790cb6694dc8f4a8b2e57
NardaPrin <nardaarandria@gmail.com>
2023-08-07 18:33:04 +0300

    Update SessionManager.java

--- Mada_tour_front/app/src/main/java/com/example/mada_tour/utils/SessionManager.java
+++ Mada_tour_front/app/src/main/java/com/example/mada_tour/utils/SessionManager.java
@@ -2,18 +2,28 @@ package com.example.mada_tour.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.widget.Toast;
 
 public class SessionManager {
 
-    private static final String SHARED_PREF_NAME = "my_shared_pref";
+    private static final String SHARED_PREF_NAME = "MyPrefs";
     private static final String KEY_TOKEN = "token";
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
-
-    public SessionManager(Context context) {
+    private Context context;
+    public SessionManager(Context ctx) {
+        context = ctx;
         sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
+    }
+    public boolean isConnected(){
+        if(getToken()!=null&&getToken()!=""){
+            System.out.println("TOKEN"+getToken());
+            return true;
+        }else{
+            return false;
+        }
     }
 
     // Enregistrer le token dans SharedPreferences
@@ -30,7 +40,8 @@ public class SessionManager {
 
     // Supprimer le token et déconnecter l'utilisateur
     public void disconnect() {
-        editor.remove(KEY_TOKEN);
+        editor.clear();
         editor.apply();
+        Toast.makeText(context, "Déconnecté", Toast.LENGTH_SHORT).show();
     }
 }
