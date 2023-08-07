7c6b5f0d1391df49cfdcc2e33e976f27cf55e479
ranoelison <ranoelisonfiononana@gmail.com>
2023-08-07 23:21:59 +0300

    fix 3

--- Mada_tour_back/app.js
+++ Mada_tour_back/app.js
@@ -24,8 +24,8 @@ const db = require("./config/db");
 //---
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
